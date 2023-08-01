3ef34eb7cdb61d0d0cf86d1b274ee4e9f39a41e4
Truumeister <126129953+Truumeister@users.noreply.github.com>
2023-08-01 15:38:44 -0700

    Add files via upload

--- counter.js
+++ counter.js
@@ -1,14 +1,23 @@
+//let counter = 0;
+if(!localStorage.getItem('counter')) {
+    localStorage.setItem('counter', 0);
 
-let counter = 0;
-
+}
 function count () {
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML= counter;
+    localStorage.setItem('counter', counter);
+    /*if (counter % 10 === 0) {
+        
 
-    if (counter % 10 === 0) {
+        
         alert(`Count is now ${counter}`);
-    }
+    }*/
 }
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('button').onclick = count;
+    document.querySelector('h1').innerHTML = localStorage.getItem('counter');
+   // setInterval(count, 1000); <provides second interval.)
+
 });
